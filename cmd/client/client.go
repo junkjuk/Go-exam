7d813b245ac49572e0c621018b2600b9bd0c5e1e
Volodymyr Kosinskyi <junkjuk5645@gmail.com>
2023-06-21 09:56:32 +0300

    Add client

---
+++ cmd/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+var url = "http://localhost:8090/sum"
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	client := new(http.Client)
+	client.Timeout = 10 * time.Second
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		resp, err := client.Post(url, "application/json", bytes.NewBuffer([]byte(text)))
+		if err != nil {
+			log.Printf("error %s", err)
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("error %s", err)
+		}
+		resp.Body.Close()
+
+		fmt.Println(string(body))
+	}
+}
